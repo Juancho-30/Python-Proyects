89f8a1ed00b2034483df5f33a904096f2247f3f1
Juan Carlos Escalante <59323894+Juancho953003@users.noreply.github.com>
2020-05-03 12:51:41 -0500

    Revert "Master"

--- Python_Basics/Ejemplo.py
+++
@@ -1,6 +0,0 @@
-with open('files/data.txt', "a+" ) as datafile: #si lo abres con append, el cursor se va al ultimo
-    datafile.seek(0)
-    content = datafile.read()
-    datafile.seek(0)
-    datafile.write(content)
-    datafile.write(content)
--- Python_Basics/charactercounter.py
+++
@@ -1,6 +0,0 @@
-def foo(character, filepath="bear.txt"):
-    file = open(filepath)
-    content = file.read()
-    return content.count(character)
-
-#Counts the number of characters from a given argument, in a .txt
