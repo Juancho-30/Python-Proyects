7ac1599e98d2859293516445baa9cddbd601741c
Juan C. Escalante <juan.escalantedo@udlap.mx>
2020-05-17 17:26:24 -0500

    Desktop-Databaseapp OOP

--- Juanchos-lair/Desktop-Databaseapp/Frontend.py
+++ Juanchos-lair/Desktop-Databaseapp/Frontend.py
@@ -1,27 +1,29 @@
 from tkinter import *
-import backend
+from backend import Database
+
+database=Database("books.db")
 
 def view_command():
    list1.delete(0,END)
-   for row in backend.view():
+   for row in database.view():
       list1.insert(END, row) #END as index for row to put the new rows at the end always
 
 def seach_command():
    list1.delete(0,END)
-   for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+   for row in database.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
       list1.insert(END,row)
 
 
 def command_entry():
-   backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+   database.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
    list1.delete(0,END)
    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
 
 def delete_command():
-   backend.delete(selected_tuple[0])
+   database.delete(selected_tuple[0])
 
 def update_command():
-   backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+   database.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
 
 
 
@@ -29,7 +31,6 @@ def get_selected_row(event):
    try:
       global selected_tuple
       index=list1.curselection()[0] #curseselection method from listbox
-      print(index)
       selected_tuple=list1.get(index)
       e1.delete(0,END)
       e1.insert(END,selected_tuple[1])
